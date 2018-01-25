d90652853530ed544648b0223b1e2402f42eec54
Argall, Nathan <Nathan.Argall@gmail.com>
2018-01-25 15:29:08 -0600

    keeping tile active, instead unchecking renderer, deactivating/activating all child directions instead

--- Assets/Scripts/TileLogic.cs
+++ Assets/Scripts/TileLogic.cs
@@ -2,60 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileLogic : MonoBehaviour {
+public class TileLogic : MonoBehaviour
+{
 
-	public static Dictionary<string, int> connections;
-	public int numConnectionsNeeded;
-	public Collider north;
-	public Collider northEast;
-	public Collider east;
-	public Collider southEast;
-	public Collider south;
-	public Collider southWest;
-	public Collider west;
-	public Collider northWest;
-	
-	void Start () 
-	{
-		gameObject.SetActive(false);
-		connections.Add("horizontal", 0);
-		connections.Add("vertical", 0);
-		connections.Add("diagRight", 0);
-		connections.Add("diagLeft", 0);
-	}
+    public static Dictionary<string, int> connections;
+    public int numConnectionsNeeded;
+    public Collider north;
+    public Collider northEast;
+    public Collider east;
+    public Collider southEast;
+    public Collider south;
+    public Collider southWest;
+    public Collider west;
+    public Collider northWest;
+    GameObject[] allDirections;
 
-	void OnMouseDown()
-	{
-		gameObject.SetActive(true);
-		Debug.Log("Tile activated!");
-		bool horizontalRowFound = isRowAttained(west, east);
-		bool verticalRowFound = isRowAttained(north, south);
-		bool diagRightRowFound = isRowAttained(southWest, northEast);
-		bool diagLeftRowFound = isRowAttained(northWest, southEast);
-		
-		if(horizontalRowFound || verticalRowFound || diagRightRowFound || diagLeftRowFound)
-		{
-			Debug.Log("Tic-tac-toe, " + numConnectionsNeeded + " in a row!");
-		}
-	}
+    void Start()
+    {
+        allDirections = new GameObject[] { north.gameObject, northEast.gameObject, 
+		east.gameObject, southEast.gameObject, south.gameObject, southWest.gameObject, 
+		west.gameObject, northWest.gameObject };
+        foreach (GameObject direction in allDirections)
+        {
+            direction.SetActive(false);
+        }
 
-	bool isRowAttained(Collider direction1, Collider direction2)
-	{
-		if(direction1 && direction2)
-		{
-			connections[direction1.tag] += 2;
-		} 
-		else if(direction1 || direction2)
-		{
-			connections[direction1.tag] += 1;
-		}
+        connections = new Dictionary<string, int>();
+        connections.Add("horizontal", 0);
+        connections.Add("vertical", 0);
+        connections.Add("diagRight", 0);
+        connections.Add("diagLeft", 0);
+    }
 
-		if(connections[direction1.tag] >= numConnectionsNeeded)
-		{
-			return true;
-		}
+    void OnMouseDown()
+    {
+        foreach (GameObject direction in allDirections)
+        {
+            direction.SetActive(true);
+        }
+        Debug.Log("Tile activated!");
 
-		return false;
-	}
+        bool horizontalRowFound = isRowAttained(west, east);
+        bool verticalRowFound = isRowAttained(north, south);
+        bool diagRightRowFound = isRowAttained(southWest, northEast);
+        bool diagLeftRowFound = isRowAttained(northWest, southEast);
+
+        if (horizontalRowFound || verticalRowFound || diagRightRowFound || diagLeftRowFound)
+        {
+            Debug.Log("Tic-tac-toe, " + numConnectionsNeeded + " in a row!");
+        }
+    }
+
+    bool isRowAttained(Collider direction1, Collider direction2)
+    {
+        if (direction1 && direction2)
+        {
+            connections[direction1.tag] += 2;
+        }
+        else if (direction1 || direction2)
+        {
+            connections[direction1.tag] += 1;
+        }
+
+        if (connections[direction1.tag] >= numConnectionsNeeded)
+        {
+            return true;
+        }
+
+        return false;
+    }
 
 }
