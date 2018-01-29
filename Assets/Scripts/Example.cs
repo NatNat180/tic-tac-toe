c7d7ac773a61a9ce901d363f1df6e2269ae32817
Argall, Nathan <Nathan.Argall@gmail.com>
2018-01-29 14:29:47 -0600

    even more modification of comment logic

--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/* --- Logic for rows ---
+/* 
+
+--- Logic for rows ---
 n = number of columns and rows
-i = starting tile
+i always starts on top row
 
-   Horizontal: (i + 1)
-   Vertical: (i + n)
-   diagRight: (i + (n - 1))
-   diagLeft: (i + (n + 1))
+   Horizontal: i = (i + 1)
+   Vertical: i = (i + n)
+   diagRightToLeft: i = (i + (n - 1))  
+   diagLeftToRight: i = (i + (n + 1))
 
-Any one of these equations must be done 'n' amount of times
-   -----------------------
-   All of this assumes that the number of rows and columns (n) are the exact same so it may not hit every use case,
-   but frankly, they should be the same number for a regular game (i.e. 3 x 3 and NOT 3 x 4 or something similar). 
-   With that assumption, implementing these equations into functions should make the game scalable.
+- Each equation must be done 'n' amount of times 
+- For the diagnal integers, we need to ensure that first iteration begins at rightmost or leftmost spot of grid
+-----------------------
+All of this assumes that the number of rows and columns (n) are the exact same so it may not hit every use case,
+but frankly, they should be the same number for a regular game (i.e. 3 x 3 and NOT 3 x 4 or something similar). 
+With that assumption, implementing these equations into functions should make the game scalable.
+ 
  */
 public class Example : MonoBehaviour
 {
@@ -39,8 +43,8 @@ public class Example : MonoBehaviour
         }
     }
 
-    /* For finding a horizontal row, we only need to find (n +/- 1) * n.
-	Therefore, we can simply iterate through the multi-dim array sequentially, 
+    /* For finding a horizontal row, from the equation above, we can 
+	simply iterate through the multi-dimensional array sequentially, 
 	and count if all objects within a given row (i) are active. */
     bool horizRowMade(GameObject[,] grid)
     {
