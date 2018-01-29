9a8c8a246df1dd56429e93663ccb275716e40ddb
Argall, Nathan <Nathan.Argall@gmail.com>
2018-01-29 11:50:54 -0600

    begin work on homegrown, algorithmic solution to finding rows - horizontal logic

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour 
+{
+	public int gridNum;
+	public GameObject[,] grid;
+	public int numActiveTilesNeeded;
+
+	void Start () 
+	{
+		grid = new GameObject[gridNum, gridNum];
+	}
+	
+	/* Will add functions for vertical, diagLeft and diagRight rows later */
+	void OnMouseDown () 
+	{
+		bool isHorizontalRow = horizRowMade(grid);
+
+		if (isHorizontalRow)
+		{
+			Debug.Log("Tic-tac-toe, " + numActiveTilesNeeded + " in a row!");
+		}
+	}
+
+	/* For finding a horizontal row, we only need to find (n +/- 1) * n.
+	Therefore, we can simply iterate through the multi-dim array sequentially, 
+	and count if all objects within a given row (i) are active. */
+	bool horizRowMade(GameObject[,] grid)
+	{
+		for (int i = 0; i < grid.Length; i++)
+		{
+			int activeTiles = 0;	
+
+			for (int j = 0; j < grid.GetLength(i); j++)
+			{
+				if (grid[i, j].activeSelf)
+				{
+					activeTiles += 1;
+					
+					if (activeTiles >= numActiveTilesNeeded) { return true; }
+				}
+			}
+		}
+
+		return false;
+	}
+}
