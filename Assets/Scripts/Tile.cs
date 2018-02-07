959a2d6dfce294c217a540a8e662186e5d8e9f4a
AndrewJeannett <andrewjeannett@gmail.com>
2018-02-06 22:59:41 -0600

    xs and os

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,26 +5,31 @@ using UnityEngine;
 public class Tile : MonoBehaviour {
 
 	public static bool lastTileX;
+    public GameObject xObj;
+    public GameObject oObj;
 
-	void Start () 
+    void Start () 
 	{
 		gameObject.tag = "isInactive";
 	}
 
     void OnMouseDown()
     {
+        Vector3 piecePos= new Vector3(transform.position.x, transform.position.y + .1f, transform.position.z);
 		if (gameObject.tag == "isInactive") 
 		{
 			if (lastTileX)
 			{
 				gameObject.tag = "isOActive";
 				lastTileX = false;
+                Instantiate(oObj, piecePos, transform.rotation);
 			} 
 			else
 			{
 				gameObject.tag = "isXActive";
 				lastTileX = true;
-			}
+                Instantiate(xObj, piecePos, Quaternion.Euler(90,45,0));
+            }
 		}
 		Debug.Log("Tile activated! = " + gameObject.tag);
     }
